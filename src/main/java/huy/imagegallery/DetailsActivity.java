273cbb0752eb03ee72462dce20691098ef4e3615
huy1395 <DoomLord1395@gmail.com>
2016-11-16 09:53:12 +0700

    Add files via upload

---
+++ src/main/java/huy/imagegallery/DetailsActivity.java
@@ -0,0 +1,30 @@
+package huy.imagegallery;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by laptop on 11/14/2016.
+ */
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.details_activity);
+
+        String title = getIntent().getStringExtra("title");
+        Bitmap bitmap = getIntent().getParcelableExtra("image");
+
+        TextView titleTextView = (TextView) findViewById(R.id.title);
+        titleTextView.setText(title);
+
+        ImageView imageView = (ImageView) findViewById(R.id.image);
+        imageView.setImageBitmap(bitmap);
+    }
+
+}
